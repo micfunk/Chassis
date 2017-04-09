bacc6483436fbd8204582934906684c29c3788b3
Mic Funk <micfunk@bendyworks.com>
2017-04-09 11:37:49 -0500

    add composer install to shell script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# A simple setup script for my needs. Want to fix this up in the future. 
+# A simple setup script for my needs. Want to fix this up in the future.
 
 # remove wp directory
 rm -rf wp
 
-# remove content directory 
+# remove content directory
 rm -rf content
 
 # add phpMyAdmin
 git clone --recursive git@github.com:Chassis/phpMyAdmin.git extensions/phpmyadmin
 
 # add MailHog
-git clone --recursive git@github.com:Chassis/MailHog.git extensions/phpmyadmin
+git clone --recursive git@github.com:Chassis/MailHog.git extensions/mailhog
